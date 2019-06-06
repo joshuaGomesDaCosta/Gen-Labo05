fdd8c806081969585ee72f062ad69c0b5a7123f9
Joshua Gomes da Costa <joshua.gomesdacosta@heig-vd.ch>
2019-06-06 16:49:25 +0200

    first tests

--- tests/CustomerTest.cc
+++ tests/CustomerTest.cc
@@ -1,9 +1,41 @@
 
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
-
+#include <string.h>
 
 #include "../src/Customer.h"
-TEST(TestSuitName, TestName) {
-	ASSERT_TRUE(true);
+
+using namespace std;
+/*
+class CustomerTest : public ::testing::Test {
+protected:
+	virtual void SetUp() {
+		Customer c;
+	}
+ 
+ virtual void TearDown() {}
+};
+*/
+
+TEST(statementIntegration, testReturnForEmptyRentals) {
+	Customer c;
+	string statement = c.statement();
+	ASSERT_EQ(statement, "Rental Record for \nAmount owed is 0\nYou earned 0 frequent renter points");
+}
+
+TEST(statementIntegration, testReturnForOneRentals) {
+	Customer c;
+	c.addRental(Rental(Movie("Avatar"),10));
+	string statement = c.statement();
+	ASSERT_EQ(statement, "Rental Record for \n\tAvatar\t14\nAmount owed is 14\nYou earned 1 frequent renter points");
+}
+
+TEST(statementIntegration, testReturnForManyRentalsWithDifferentPriceCode) {
+	Customer c;
+	c.addRental(Rental(Movie("Shrek",Movie::CHILDRENS),8));
+	c.addRental(Rental(Movie("Inception"),12));
+	c.addRental(Rental(Movie("Avatar2",Movie::NEW_RELEASE),20));
+	string statement = c.statement();
+
+	ASSERT_EQ(statement, "Rental Record for \n\tShrek\t9\n\tInception\t17\n\tAvatar2\t60\nAmount owed is 86\nYou earned 4 frequent renter points");
 }
