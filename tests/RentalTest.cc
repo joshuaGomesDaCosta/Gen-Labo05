7209d89deec0516944ad44be4149e01ca7ad615b
Boosterloop <alison.savary@me.com>
2019-06-15 19:28:46 +0200

    RenatlTest.cc with mocks for Movie

--- tests/RentalTest.cc
+++ tests/RentalTest.cc
@@ -2,40 +2,79 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 #include <string.h>
-#include <src/Rental/MovieImpl.h>
 #include <src/Rental/RentalImpl.h>
+#include <src/Rental/MovieMock.h>
 
 #include "../src/Rental/Rental.h"
 #include "src/Rental/MoviePriceCode/NewRelease.h"
 #include "src/Rental/MoviePriceCode/Regular.h"
 #include "src/Rental/MoviePriceCode/Children.h"
 
+using namespace std;
+using ::testing::Return;
+
 TEST(determineAmountsIntegration, testReturnForRentalWithRegularMovie) {
-	RentalImpl rental = RentalImpl(std::shared_ptr<Movie>(new MovieImpl("Avatar", std::shared_ptr<MoviePriceCode>(new Regular()))),10);
+
+    shared_ptr<MovieMock> movie(new MovieMock);
+    shared_ptr<MoviePriceCode> regular(new Regular());
+
+    EXPECT_CALL(*movie, getPriceCode()).WillRepeatedly(Return(regular));
+
+	RentalImpl rental = RentalImpl(movie, 10);
 	ASSERT_EQ(rental.determineAmount(), 14);
 }
 
 TEST(determineAmountsIntegration, testReturnForRentalWithReleaseMovie) {
-	RentalImpl rental = RentalImpl(std::shared_ptr<Movie>(new MovieImpl("Avatar", std::shared_ptr<MoviePriceCode>(new NewRelease()))),10);
+
+    shared_ptr<MovieMock> movie(new MovieMock);
+    shared_ptr<MoviePriceCode> newRelease(new NewRelease());
+
+    EXPECT_CALL(*movie, getPriceCode()).WillRepeatedly(Return(newRelease));
+
+	RentalImpl rental = RentalImpl(movie, 10);
 	ASSERT_EQ(rental.determineAmount(), 30);
 }
 
 TEST(determineAmountsIntegration, testReturnForRentalWithChildrensMovie) {
-	RentalImpl rental = RentalImpl(std::shared_ptr<Movie>(new MovieImpl("Avatar",std::shared_ptr<MoviePriceCode>(new Children()))),10);
+
+    shared_ptr<MovieMock> movie(new MovieMock);
+    shared_ptr<MoviePriceCode> children(new Children());
+
+    EXPECT_CALL(*movie, getPriceCode()).WillRepeatedly(Return(children));
+
+	RentalImpl rental = RentalImpl(movie, 10);
 	ASSERT_EQ(rental.determineAmount(), 12);
 }
 
 TEST(getFrequentRenterPoints, testReturnTheNumberOfFrequentRenterPointsWithChildren) {
-    RentalImpl rental = RentalImpl(std::shared_ptr<Movie>(new MovieImpl("Avatar",std::shared_ptr<MoviePriceCode>(new Children()))),10);
+
+    shared_ptr<MovieMock> movie(new MovieMock);
+    shared_ptr<MoviePriceCode> children(new Children());
+
+    EXPECT_CALL(*movie, getPriceCode()).WillRepeatedly(Return(children));
+
+    RentalImpl rental = RentalImpl(movie, 10);
     ASSERT_EQ(rental.getFrequentRenterPoints(), 1);
 }
 
 TEST(getFrequentRenterPoints, testReturnTheNumberOfFrequentRenterPointsWithNewReleaseWithMultipleDays) {
-    RentalImpl rental = RentalImpl(std::shared_ptr<Movie>(new MovieImpl("Avatar",std::shared_ptr<MoviePriceCode>(new NewRelease()))),2);
+
+    shared_ptr<MovieMock> movie(new MovieMock);
+    shared_ptr<MoviePriceCode> newRelease(new NewRelease());
+
+    EXPECT_CALL(*movie, getPriceCode()).WillRepeatedly(Return(newRelease));
+
+    RentalImpl rental = RentalImpl(movie, 2);
     ASSERT_EQ(rental.getFrequentRenterPoints(), 2);
 }
 
 TEST(getFrequentRenterPoints, testReturnTheNumberOfFrequentRenterPointsWithNewReleaseWithOneDay) {
-    RentalImpl rental = RentalImpl(std::shared_ptr<Movie>(new MovieImpl("Avatar",std::shared_ptr<MoviePriceCode>(new NewRelease()))),1);
+
+    shared_ptr<MovieMock> movie(new MovieMock);
+    shared_ptr<MoviePriceCode> newRelease(new NewRelease());
+
+    EXPECT_CALL(*movie, getPriceCode()).WillRepeatedly(Return(newRelease));
+
+    RentalImpl rental = RentalImpl(movie, 1);
     ASSERT_EQ(rental.getFrequentRenterPoints(), 1);
 }
